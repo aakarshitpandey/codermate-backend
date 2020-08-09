643a8c6be397266474982b976317dd6a6648cfdf
aakarshitpandey <aakarshit.pandey7@gmail.com>
2020-08-09 14:18:35 +0530

    added service account

--- server/firebase/config.js
+++ server/firebase/config.js
@@ -1,4 +1,17 @@
 import * as firebase from "firebase/app";
 
+import admin from 'firebase-admin'
 import "firebase/auth";
 import "firebase/firestore";
+
+import * as firebaseConfig from '../../firebase.json'
+import * as serviceAccountConfig from '../../firebaseServiceAcct.json'
+
+firebase.initializeApp(firebaseConfig);
+
+(function setUpServiceAcct() {
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccountConfig),
+        databaseURL: "https://chatbot-8510b.firebaseio.com"
+    })
+})()
